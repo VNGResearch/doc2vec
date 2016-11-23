3285171a27f7a0b9aebbdbf1ee05887004536ad1
Thanh Cong Le <thanhlct@gmail.com>
2016-11-23 09:59:53 +0100

    Add bag of words model and first time get medium result 50% in test set

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 - try other approaches: rnn, bag-of-words, baiesian
 
 '''
+#TODO: Vietnamese tokenizer, split mark (e.g. ", .!~ from words
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -33,6 +34,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.preprocessing import StandardScaler
 
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
 #from config import config
 
 import pdb
@@ -47,7 +50,12 @@ class Log(object):
     def warn(sender, message):
         print('---WARN: ', sender, message, sep='\t')
 
+    def error(sender, message):
+        sys.stderr.write('---ERROR: ', sender, message, '\n', sep='\t')
+        
+
 class Doc2Vec(object):
+    '''Neural Network-based method.'''
     def __init__(self, dm=1, size=100, window=8, min_count=5, passes=1, batch_size=0, shuffle=False, dm_mean=0, dm_concat=0):
         self.dm = dm
         self.size = size
@@ -118,19 +126,33 @@ class Doc2Vec(object):
         Args:
             X: Document iterator.
         '''
+
+        if 'batch_size' in kwargs:
+            self.batch_size = kwargs['batch_size']
+        partial_train = False#train in addational data #something already existed e.g. vocabs
+        if 'partial_train' in kwargs and kwargs['partial_train']:
+            partial_train = True 
+        
+        if 'shuffle' in kwargs and kwargs['shuffle']:
+            Log.info(self, '----shuffle mode')
+            self.shuffle = True
+
+
         Log.info(self, '======Training doc2vec model====')
-        model = self.load()
+        if not partial_train:
+            model = self.load()
 
-        if model is not None:
-            self.model = model
-            return self
+            if model is not None:
+                self.model = model
+                return self
 
-        Log.info(self, '--build vocabs')
-        self.model.build_vocab(X)
+        if len(self.model.vocab)==0:
+            Log.info(self, '--build vocabs')
+            self.model.build_vocab(X)
 
         #mutiple passes, mini-batch training
         for rep in range(0, self.passes):
-            Log.info(self, '==========pass num{}'.format(rep))
+            Log.info(self, '==========internal pass num{}'.format(rep))
             corpus = []
             for doc in X:
                 if rep==0 and doc.tags[0] not in self.doc_infos:#training doc information
@@ -140,16 +162,19 @@ class Doc2Vec(object):
                 if self.batch_size > 0 and len(corpus)>=self.batch_size:
                     if self.shuffle:
                         random.shuffle(corpus)
-                    self.model.train(corpus)
                     Log.info(self, '---train a mini-batch')
+                    self.model.train(corpus)
+                    corpus = []
             if len(corpus) !=0:
+                if self.shuffle:
+                    random.shuffle(corpus)
                 Log.info(self, '---train the last')
-                #self.model.train(corpus)
+                self.model.train(corpus)
 
         self.model.save(self.auto_name())
         self.save_docinfo()
 
-        self.visualize(self.model.docvecs, corpus, self.auto_name(),500) 
+        #self.visualize(self.model.docvecs, corpus, self.auto_name(),500) 
         return self
 
     def load_docinfo(self):
@@ -169,7 +194,26 @@ class Doc2Vec(object):
         return self.model.infer_vector(words)
 
     def most_similar(self, docvec, topn = 10):
-        return self.model.docvecs.most_similar(positive=[docvec], topn=topn) 
+        return self.model.docvecs.most_similar(positive=[docvec], topn=topn)
+
+
+class BWDoc2Vec(object):
+    def doc_iter(self, docs):
+        for doc in docs:
+            yield ' '.join(doc.words)
+            
+    def train(self, docs, tfidf=False):
+        if tfidf:
+            self.doc2vec = TfidfVectorizer()
+        else:
+            #count_vec = CountVectorizer(tokenizer=lambda x: x.split())#TODO: paramerters for optimization???
+           self.doc2vec = CountVectorizer(min_df=0.01, max_df=0.8)#TODO: paramerters for optimization???
+        self.doc2vec.fit(self.doc_iter(docs))
+        print('See difference of models')
+        pdb.set_trace()
+
+    def infer_docvec(self, words):
+        return self.doc2vec.transform([' '.join(words)]).toarray()[0]
 
 class Classifier(object):
     def __init__(self, doc2vec):
@@ -266,16 +310,17 @@ class MultipClassifiers(Classifier):
     def __init__(self, doc2vec):
         super(MultipClassifiers, self).__init__(doc2vec)
         self.classifiers = [
-            #KNeighborsClassifier(3),
-            #SVC(kernel="linear", C=0.025),
-            SVC(gamma=2, C=1),
-            #GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+            #KNeighborsClassifier(3), #*
+            KNeighborsClassifier(6), #*
+            SVC(kernel="linear", C=0.025),
+            #SVC(gamma=2, C=1), #*
+            #GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),#need memory
             #DecisionTreeClassifier(max_depth=5),
             #RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
             #MLPClassifier(alpha=1),
             #AdaBoostClassifier(),
-            #GaussianNB(),
-            #QuadraticDiscriminantAnalysis()
+            GaussianNB(),
+            #QuadraticDiscriminantAnalysis() #*
             ]
 
     def get_data(self, train_docs):
@@ -298,9 +343,10 @@ class MultipClassifiers(Classifier):
         for cls in self.classifiers:
             Log.info(self, 'fit ' + cls.__class__.__name__)
             cls.fit(X, y)
-            Log.info(self, 'score ')
+            Log.info(self, 'score in train set')
             score = cls.score(X, y)
             Log.info(self, '!!!RESULT!!in train!{}:{}'.format(cls.__class__.__name__, score))
+            #Log.error(self, '!!!RESULT!!in train!{}:{}'.format(cls.__class__.__name__, score))
     
     def predict(self, doc_words):
         x = self.doc2vec.infer_docvec(doc_words)
@@ -322,6 +368,7 @@ class MultipClassifiers(Classifier):
             Log.info(self, 'score ' + cls.__class__.__name__)
             pres = cls.predict(X)
             correct[cls.__class__.__name__] = sum(pres==y)/float(total)
+            Log.info(self, 'acc={}'.format(correct[cls.__class__.__name__]))
         return correct 
         
     def score2(self, train_docs):
@@ -360,7 +407,7 @@ def read_corpus(data_dir, from_percent, to_percent):
         topic_id += 1
         doc_count = -1
         with open(filename) as f:
-            Log.info('read_corpus', '{}\t{}'.format(topic_id, os.path.basename(filename)))
+            #Log.info('read_corpus', '{}\t{}'.format(topic_id, os.path.basename(filename)))
             #TODO seek to the right part for reading
             docs = f.readlines()
             doc_len = float(len(docs))
@@ -374,19 +421,40 @@ def read_corpus(data_dir, from_percent, to_percent):
 
                 doc_id += 1
                 parts = doc.split('\t')
-                words = ' '.join(part.strip() for part in parts[1:])
-                #words = gensim.utils.to_unicode(words).split()
+                words = ' '.join(part.strip() for part in parts[1:])#concat title, descrpition, content and labels
+                if token_type == 'word':
+                    words = gensim.utils.to_unicode(words).split()
+                if token_type == 'vi_token':
+                    raise NotImplementedError() 
                 #pdb.set_trace()
                 #corpus.append(Document(parts[0], topic_id, doc_count, words, [doc_id]))
                 yield Document(parts[0], topic_id, doc_count, words, [doc_id], os.path.basename(filename))
     #return corpus
 
+@multigen
+def read_addational_corpus(data_dir, doc_id=-1):
+    for filename in glob.iglob(data_dir + '*.txt'):
+        doc_count = -1
+        with open(filename) as f:
+            Log.info('read_corpus', '{}'.format(os.path.basename(filename)))
+            for doc in f:
+                doc_count +=1
+                doc_id +=1
+                words = doc.strip()#concat title, descrpition, content and labels
+                if token_type == 'word':
+                    words = gensim.utils.to_unicode(words).split()
+                if token_type == 'vi_token':
+                    raise NotImplementedError() 
+                yield Document(None, None, doc_count, words, [doc_id], 'wiki')
+
+
 #============global configuration
 data_dir = '../crawl_news/data/zing/'
+add_data_dir = '../crawl_news/data/wiki/'
 train_percent = 0.6
 valid_percent = 0.2
 test_percent = 0.2
-token_type = 'char'#word, vn_token
+token_type = 'word'#word, vn_token
 model_dir = './models/'
 
 params = {
@@ -433,21 +501,68 @@ def run1():
 
 def run2():
     train_docs = read_corpus(data_dir, 0, train_percent)
-    doc2vec = Doc2Vec()
-    doc2vec.train(train_docs)
-    
+    train_small = read_corpus(data_dir, 0.1, 0.3)
+    test_small = read_corpus(data_dir, train_percent, train_percent + 0.1)
+    test = read_corpus(data_dir, train_percent, 1.0)
+
+    wiki_docs = read_addational_corpus(add_data_dir, doc_id = 39911)
+
+    #test_docs = read_corpus(data_dir, train_percent, 1.0)
+
+    doc2vec = Doc2Vec(size=100)
+    doc2vec.train(train_docs, shuffle=True)
+    '''
+    print('=================fit and avaluate classification')
     cls = MultipClassifiers(doc2vec)
     cls.fit(train_docs)
 
-    test_docs = read_corpus(data_dir, train_percent, 1.0)
-    accs = cls.score(test_docs)
+    accs = cls.score(test)
     print(accs)
+    '''
+
+    for rep in range(13):
+        print('===========================pass {}'.format(rep))
+        #doc2vec.train(train_docs)
+        doc2vec.train(train_docs, partial_train = True, shuffle=True)
+        #print('=================train addation data')
+        #doc2vec.train(wiki_docs, batch_size=50000, partial_train=True, shuffle=False)
+    
+        if rep%3==0:
+            print('=================fit and avaluate classification')
+            cls = MultipClassifiers(doc2vec)
+            cls.fit(train_docs)
+
+            accs = cls.score(test)
+            print(accs)
     print('Done')
     pdb.set_trace()
 
+    #TODO next, change size to 300, then shuffle
+
+def run3():
+    train_full = read_corpus(data_dir, 0, train_percent)
+    test_full = read_corpus(data_dir, train_percent, 1.0)
+    train_small = read_corpus(data_dir, 0.1, 0.3)
+    test_small = read_corpus(data_dir, train_percent, train_percent + 0.1)
+
+    train = train_full
+    test = test_full
+    
+    doc2vec = BWDoc2Vec()
+    print('===================fit dwdoc2vec---')
+    doc2vec.train(train)
+
+    cls = MultipClassifiers(doc2vec)
+    print('===================fit classifiers---')
+    cls.fit(train)
+
+    print('===================score classifiers---')
+    print(cls.score(test))
+
 def main():
-    #run1()
-    run2()
+    #run1()#GridSearch for hyperparameters for nueral-based Doc2Vec
+    run2()#for neural-based Doc2Vec
+    #run3()#for Bag of Word
 
 if __name__=='__main__':
     main()
